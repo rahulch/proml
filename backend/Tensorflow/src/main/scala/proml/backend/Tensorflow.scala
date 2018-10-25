e24f8095ac1c7ee04a3bd69975ec6cdb6c6cecbc
rahulch <rahul.ch@gmail.com>
2018-10-25 12:22:35 -0700

    add Tensorflow Backend

---
+++ backend/Tensorflow/src/main/scala/proml/backend/Tensorflow.scala
@@ -0,0 +1,40 @@
+package proml.backend
+
+import org.platanios.tensorflow.api.tensors.Tensor
+import org.platanios.tensorflow.api._
+import org.platanios.tensorflow.api.tensors.ops.Math._
+import proml.Backend
+
+object Tensorflow {
+  type TypedTensor[A] = Tensor
+  implicit val tensorInstance = new Backend[TypedTensor[Double], TypedTensor[Boolean]]{
+    implicit def tensorToMathOps(value: tensors.Tensor): MathOps = MathOps(value)
+    override def plus(x: TypedTensor[Double], y: TypedTensor[Double]): TypedTensor[Double] = x + y
+    override def plus(x: TypedTensor[Double], y: Double): TypedTensor[Double] = x + y
+    override def minus(x: TypedTensor[Double], y: TypedTensor[Double]): TypedTensor[Double] = x - y
+    override def minus(x: TypedTensor[Double], y: Double): TypedTensor[Double] = x - y
+    override def times(x: TypedTensor[Double], y: TypedTensor[Double]): TypedTensor[Double] = x * y
+    override def times(x: TypedTensor[Double], y: Double): TypedTensor[Double] = x * y
+    override def divide(x: TypedTensor[Double], y: TypedTensor[Double]): TypedTensor[Double] = x / y
+    override def divide(x: TypedTensor[Double], y: Double): TypedTensor[Double] = x / y
+    override def divide(x: Double, y: TypedTensor[Double]): TypedTensor[Double] = x/y
+    override def negate(x: TypedTensor[Double]): TypedTensor[Double] = -x
+    override def rand(x: TypedTensor[Double]): TypedTensor[Double] = Tensor.rand(x.dataType, x.shape)
+    override def randNormal(x: TypedTensor[Double]): TypedTensor[Double] = Tensor.rand(x.dataType, x.shape)
+    override def zero(x: TypedTensor[Double]): TypedTensor[Double] = Tensor.zeros(x.dataType, x.shape)
+    override def one(x: TypedTensor[Double]): TypedTensor[Double] = Tensor.ones(x.dataType, x.shape)
+    override def power(x: TypedTensor[Double], y: Double): TypedTensor[Double] = x ^ y
+    override def greater(x: TypedTensor[Double], y: TypedTensor[Double]): Tensor = x > y
+    override def greater(x: TypedTensor[Double], y: Double): Tensor = x > y
+    override def not(x: TypedTensor[Boolean]): Tensor = !x
+    override def size(x: TypedTensor[Double]): Long = x.size
+    override def sumOnShape(x: TypedTensor[Double]): Double = x.sum(null, false).entriesIterator.next().asInstanceOf[Double]
+    override def log(x: TypedTensor[Double]): TypedTensor[Double] = log1p(x)
+    override def exp(x: TypedTensor[Double]): TypedTensor[Double] = expm1(x)
+  }
+
+  def toString(t: Tensor) = {
+    val entries = t.entriesIterator.toList
+    s"$t:$entries"
+  }
+}
