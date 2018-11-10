597045022e7add3a0c1b0921161dfcae4ddd4e88
rahulch <rahul.ch@gmail.com>
2018-11-10 12:44:32 -0800

    add helpers

--- core/src/main/scala/proml/backend/MatrixOps.scala
+++ core/src/main/scala/proml/backend/MatrixOps.scala
@@ -10,6 +10,8 @@ trait MatrixOps[A] {
   def randNormal[M, N](m: Int, n: Int): Matrix[A, M, N]
 
   def transpose[M, N](lhs: Matrix[A, M, N]): Matrix[A, N, M]
+  def mtanh[M, N](lhs: Matrix[A, M, N]): Matrix[A, M, N]
+  def msigmoid[M, N](lhs: Matrix[A, M, N]): Matrix[A, M, N]
 
   def mdot[M, N, O](lhs: Matrix[A, M, N], other: Matrix[A, N, O]): Matrix[A, M, O]
   def mpls[M, N](lhs: Matrix[A, M, N], other: Matrix[A, M, N]): Matrix[A, M, N]
@@ -20,6 +22,10 @@ trait MatrixOps[A] {
   def msub[M, N](lhs: Matrix[A, M, N], other: Double): Matrix[A, M, N]
   def mdiv[M, N](lhs: Matrix[A, M, N], other: Matrix[A, M, N]): Matrix[A, M, N]
   def mdiv[M, N](lhs: Matrix[A, M, N], other: Double): Matrix[A, M, N]
+  class Ops[M, N](m: Matrix[A, M, N]) {
+    def modot[O](n: Matrix[A, N, O]) = mdot(m, n)
+  }
+  implicit def mkDatatypeOps[M, N](lhs: Matrix[A, M, N]) = new Ops(lhs)
 }
 
 object MatrixOps {
