5322ed6c6cfedb279b454002ce464f12499e6869
rahulch <rahul.ch@gmail.com>
2018-11-09 20:27:22 -0800

    fix matrixOps

--- core/src/main/scala/proml/backend/MatrixOps.scala
+++ core/src/main/scala/proml/backend/MatrixOps.scala
@@ -3,6 +3,7 @@ import shapeless.Nat
 import shapeless.ops.nat.ToInt
 
 trait MatrixOps[A] {
+
   case class Matrix[M <: Nat, N <: Nat](underlying: A)
 
   def zeros[M <: Nat, N <: Nat](
@@ -15,37 +16,15 @@ trait MatrixOps[A] {
     mToInt: ToInt[M],
     nToInt: ToInt[N]
   ): Matrix[M, N]
-  def transpose[M <: Nat, N <: Nat](
-    implicit
-    mToInt: ToInt[M],
-    nToInt: ToInt[N]
-  ): Matrix[N, M]
-  def dott[M <: Nat, N <: Nat, O <: Nat](other: Matrix[N, O])(
-    implicit
-    mToInt: ToInt[M],
-    nToInt: ToInt[N],
-    oToInt: ToInt[O],
-  ): Matrix[M, O]
 
-  def mplus[M <: Nat, N <: Nat](other: Double)(
-    implicit
-    mToInt: ToInt[M],
-    nToInt: ToInt[N]
-  ): Matrix[M, N]
-  def mplus[M <: Nat, N <: Nat](other: Double)(
-    implicit
-    mToInt: ToInt[M],
-    nToInt: ToInt[N]
-  ): Matrix[M, N]
-  def msub[M <: Nat, N <: Nat](other: Double)(
-    implicit
-    mToInt: ToInt[M],
-    nToInt: ToInt[N]
-  ): Matrix[M, N]
-  def mdiv[M <: Nat, N <: Nat](other: Double)(
-    implicit
-    mToInt: ToInt[M],
-    nToInt: ToInt[N]
-  ): Matrix[M, N]
-
+  def transpose[M <: Nat, N <: Nat](underlying: A): Matrix[N, M]
+  def mdot[M <: Nat, N <: Nat, O <: Nat](lhs: Matrix[M, N], other: Matrix[N, O]): Matrix[M, O]
+  def mpls[M <: Nat, N <: Nat](lhs: Matrix[M, N], other: Matrix[M, N]): Matrix[M, N]
+  def mpls[M <: Nat, N <: Nat](lhs: Matrix[M, N], other: Double): Matrix[M, N]
+  def mmul[M <: Nat, N <: Nat](lhs: Matrix[M, N], other: Matrix[M, N]): Matrix[M, N]
+  def mmul[M <: Nat, N <: Nat](lhs: Matrix[M, N], other: Double): Matrix[M, N]
+  def msub[M <: Nat, N <: Nat](lhs: Matrix[M, N], other: Matrix[M, N]): Matrix[M, N]
+  def msub[M <: Nat, N <: Nat](lhs: Matrix[M, N], other: Double): Matrix[M, N]
+  def mdiv[M <: Nat, N <: Nat](lhs: Matrix[M, N], other: Matrix[M, N]): Matrix[M, N]
+  def mdiv[M <: Nat, N <: Nat](lhs: Matrix[M, N], other: Double): Matrix[M, N]
 }
